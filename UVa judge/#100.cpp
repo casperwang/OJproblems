5516cfec2334de91adc40d0d5cf465536f26c285
casperwang <casperwang2014@gmail.com>
2018-11-18 19:07:32 +0800

    cleanCode UVa#100

--- UVa judge/#100.cpp
+++ UVa judge/#100.cpp
@@ -1,33 +1,26 @@
 #include <iostream>
-
+#include <stack>
 using namespace std;
-long long int a, b, c, ans;
-int f(int num, int j) {
-    if (num == 1) {
-        c = j;
-        return 0;
-    }
-    if (num % 2 == 1) {
-        f(num * 3 + 1, j+1);
-    } else {
-        f(num / 2, j+1);
-    }
-}
-int main()
-{
+
+long long int a , b;
+int ans;
+
+int main() {
     while(cin >> a >> b) {
         ans = 0;
         cout << a << " " << b << " ";
-        if (a > b) {
-            swap(a, b);
-        }
+        if (a > b) swap(a, b);
         for (int i = a; i <= b; i++) {
-            f(i, 0);
-            if (c > ans) {
-                ans = c;
-            }
+			stack <long long int> cycle;
+        	cycle.push(i);
+        	while (cycle.top()>1) {
+        		if (cycle.top()%2) cycle.push(3*cycle.top()+1);
+    			else cycle.push(cycle.top()/2);
+			} 
+			int len = cycle.size();
+            ans = max(ans, len);
         }
-        cout << ans+1 << endl;
+        cout << ans << endl;
     }
     return 0;
 }
