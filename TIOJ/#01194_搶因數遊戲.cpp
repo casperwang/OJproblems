a411948e1a6caa3308a7e75a7caae9404ffbef93
casperwang <casperwang2014@gmail.com>
2018-12-06 18:37:23 +0800

    new TIOJ #1194

---
+++ TIOJ/#01194_搶因數遊戲.cpp
@@ -0,0 +1,15 @@
+#搶因數遊戲: trivial
+#include <iostream>
+using namespace std;
+
+string n, a, b;
+
+int main(){
+    while (cin >> n) {
+        if (n == "0") return 0;
+        cin >> a >> b;
+        cout << a << endl;
+    }
+    return 0;
+}
+
